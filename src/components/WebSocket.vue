2a54e51ac6ae2784f447d81e16765cdbba1beff2
modris <modrislauzis@gmail.com>
2024-01-22 18:12:57 +0200

    SockJS removed. Add custom heartbeats between server and client. Also if no heartbeat is received then attempt to reconnect.

--- src/components/WebSocket.vue
+++ src/components/WebSocket.vue
@@ -3,29 +3,30 @@
 </template>
 
 <script setup>
-import {ref, watch, defineProps} from 'vue';
+import { ref, watch, defineProps, nextTick} from 'vue';
 import Stomp from "webstomp-client";
 const userId = crypto.randomUUID(); // this will only work on localhost or HTTPS!!
-const url = 'http://localhost:8080/websocket'; // http link because of sockJS.
+const url = 'ws://localhost:8080/websocket'; // http link because of sockJS.
 //let options = {debug: false, protocols: Stomp.supportedProtocols()};
-let socket = new SockJS(url);
-let  stomp = Stomp.over(socket, ['v10.stomp', 'v11.stomp', 'v12.stomp']);
+let options = { debug: false, protocols: ['v12.stomp'], heartbeat: {incoming: 0, outgoing: 0} };
+let socket = new WebSocket(url);
+let  stomp = Stomp.over(socket, options);
 
 //var payload = JSON.stringify({'fen': 'k7/7Q/5Q2/8/8/8/3K4/8 w - - 0 1', 'userId': userId});
 const payload = ref('');
 const bestmove = ref('');
-
+let reconnectInterval;
 let SOCKET_CONNECTING = 0;
 let SOCKET_OPEN = 1;
 let SOCKET_CLOSING = 2;
 let SOCKET_CLOSED = 3;
-let reconnectInterval = ref(null); 
+
 
 stomp.connect('guest', 'guest', function(frame) {
     console.log("Connected!");
-    clearInterval(intervalId.value); // clears attempting to reconnect interval.
     stomp.subscribe("/topic/bestmove"+userId, handleServerResponse);
-    //sendFen();
+    setTimeout(handlePing,3000); // heartbeat every 3 seconds. Starts ping pong!
+
 });
 
 const props = defineProps({
@@ -35,6 +36,7 @@ const props = defineProps({
   fenHistoryUser: String,
   moveHistoryUser: String,
 })
+
 /*
     watch(() => props.fen, (newFen) => {
     if(newFen != 'undo'){ 
@@ -60,11 +62,25 @@ function sendFen(payload){
 
 
 const emit = defineEmits(['received-server-bestmove'])
+
 function handleServerResponse(message){
     bestmove.value = message.body;
     console.log("Received from server: "+bestmove.value)
-    emit('received-server-bestmove', bestmove.value);
+    if(bestmove.value == 'Pong'){
+        clearInterval(reconnectInterval);
+        setTimeout(handlePing,3000); // heartbeat every 3 seconds.
+    } else{
+        emit('received-server-bestmove', bestmove.value);
+    }
 
+}
+
+function handlePing(){
+    let ping = JSON.stringify({'fen': 'Ping', 'userId': userId, 'move': 'Ping'});
+    console.log("Sending Ping!");
+    stomp.send("/app/websocket", ping);
+    reconnectInterval = setInterval(reconnectingIntervalFunction, 8000); // 8 seconds before we attempt to reconnect
+    // if no pong is received it will call reconnectIntervalFunction().
 
 }
 
@@ -76,37 +92,48 @@ stomp.onStompError = (frame) => {
     console.error('Broker reported error: ' + frame.headers['message']);
     console.error('Additional details: ' + frame.body);
 };
-const intervalId = ref(null);
 
-socket.onclose= () => {
-    // reconnect now...
-    console.log("Socket closed....");
-    //stomp.disconnect(); // for proper cleanup of resources
-   // reconnectInterval.value = setInterval(reconnectingIntervalFunction, 1000);
+
+
+
+ socket.onclose= () => {
+    console.log("WebSocket closed....");
 
 }
-/*
+let reconnectIntervalAfterClosed;
   // Your function to be called at regular intervals
   let reconnectingIntervalFunction = () => {
+    clearInterval(reconnectInterval); // Clear reconnecting after 8 seconds if no ping pong (heartbeat) received.
+   // After websocket closed, reconnect every 1 second
+    reconnectIntervalAfterClosed = setInterval(websocketReconnect, 1000); 
     
-    if(socket.readyState == SOCKET_CONNECTING ||  socket.readyState == SOCKET_OPEN) {
-         // don't reconnect if we are attempting to connect
+  }
+  let websocketReconnect = () => {
+   
+    if(socket.readyState == SOCKET_CONNECTING  || socket.readyState == SOCKET_CLOSING) {
+         // don't reconnect if we are attempting to connect, connection is open or the socket is closing.
         return;
     }
     if(socket.readyState == SOCKET_CLOSED){
-        console.log("Websocket closed. Attempting to reconnect");
+        console.log("WebSocket closed. Attempting to reconnect");
+    }
+    if(socket.readyState == SOCKET_OPEN){
+        console.log("Websocket is Open. Clearing the interval value.");
+        clearInterval(reconnectIntervalAfterClosed);
     }
 
-    socket = new SockJS(url); // fresh start for clean connection
-    options = {debug: false, protocols: Stomp.VERSIONS.supportedProtocols()};
-    stomp = Stomp.over(socket, options); // fresh start for clean connection
-
+    socket = new WebSocket(url);
+    stomp = Stomp.over(socket, options);
     stomp.connect('guest', 'guest', function(frame) {
         console.log("Reconnected!");
-        clearInterval(intervalId.value); // clears attempting to reconnect interval.
         stomp.subscribe("/topic/bestmove"+userId, handleServerResponse);
+        clearInterval(reconnectIntervalAfterClosed); // clears attempting to reconnect interval.
+        setTimeout(handlePing,3000); // heartbeat every 3 seconds.
     });
+  }
 
- };
- */
+ 
+    
+
+ 
 </script>
